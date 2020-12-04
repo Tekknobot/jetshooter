b379bc893b576deae84dafafe61cb28e8bd640e1
Tekknobot <thrillatron@hotmail.com>
2020-12-04 00:38:22 -0500

    Add ExplosionTask on Boss Death.

--- Assets/Script/bossTarget.cs
+++ Assets/Script/bossTarget.cs
@@ -13,9 +13,16 @@ public class bossTarget : MonoBehaviour
     public GameObject explosionPrefab; 
 	public GameObject explosionEmitter;
 
+    public float pieceCount = 12f;
+
+    public GameObject[] oscillators;    
+
     void Start() {
 		currentHealth = maxHealth;
 		healthBar.SetMaxHealth(maxHealth);
+
+        GetComponent<ObjectOscillator>().enabled = true;
+        GetComponent<BoxCollider2D>().enabled = true;
     }
 
     void Update() {
@@ -30,7 +37,24 @@ public class bossTarget : MonoBehaviour
         healthBar.SetHealth(currentHealth);
         if (currentHealth <= 0f)
         {
-            Death();
+            InstantiateCircle();
+        }
+    }
+
+    void InstantiateCircle () 
+    {
+        StartCoroutine(ExplosionTask());
+    }
+
+    void CenterObject()
+    {
+        GetComponent<ObjectOscillator>().enabled = false;
+        GetComponent<BoxCollider2D>().enabled = false;
+
+        oscillators = GameObject.FindGameObjectsWithTag("Oscillator");
+        foreach (GameObject oscillator in oscillators)
+        {
+            oscillator.SetActive(false);
         }
     }
 
@@ -41,5 +65,29 @@ public class bossTarget : MonoBehaviour
         Instantiate(explosionPrefab, explosionEmitter.transform.position, Quaternion.identity); 
         GameObject.Find("Boss bar").SetActive(false);
         Destroy(gameObject);
+    }
+
+    IEnumerator ExplosionTask() {
+        CenterObject();
+        for (int i = 0; i < pieceCount; i++)
+        {
+            /* Distance around the circle */  
+            var radians = 2 * Mathf.PI / pieceCount * i;
+
+            /* Get the vector direction */ 
+            var vertical = Mathf.Sin(radians);
+            var horizontal = Mathf.Cos(radians);
+
+            var spawnDir = new Vector3 (horizontal, 0, vertical);
+
+            /* Get the spawn position */ 
+            var spawnPos = transform.position + spawnDir * 3f; // Radius is just the distance away from the point
+            
+            Instantiate(explosionPrefab, spawnPos, Quaternion.identity);
+            GetComponent<ShakeBehavior>().enabled = true;
+            this.gameObject.SendMessage("TriggerShake", SendMessageOptions.RequireReceiver);
+            yield return new WaitForSeconds(0.2f);
+        }    
+        Death();    
     }
 }
