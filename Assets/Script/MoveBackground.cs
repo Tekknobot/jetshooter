de5455b83c424cf8073af6410674403ee87d195b
Tekknobot <thrillatron@hotmail.com>
2020-12-08 04:55:17 -0500

    Add city background sprites.

--- Assets/Script/MoveBackground.cs
+++ Assets/Script/MoveBackground.cs
@@ -6,18 +6,21 @@ public class MoveBackground : MonoBehaviour {
  
     public float speed = 2f;
     public float yDirection = -1; //move from right to left
+    public float yPos = -10;
+    public Vector3 startPos;
+    
    
     // Use this for initialization
     void Start () {
-    
+        startPos = transform.position;
     }
      
     // Update is called once per frame
     void Update () {
         //move the root BG from right to left
         transform.Translate(new Vector3(0, speed * yDirection * Time.deltaTime, 0));
-        if (transform.position.y <= -10) {
-            transform.position = new Vector3(0,0,0);
+        if (transform.position.y <= yPos) {
+            transform.position = startPos;
         }
     }   
 }
