249a10147cb16f21e7afbe4fc040d4f7317fb766
Tekknobot <thrillatron@hotmail.com>
2020-12-08 02:21:16 -0500

    Add vertical scroll to ocean sprite.

---
+++ Assets/Script/MoveBackground.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+ 
+public class MoveBackground : MonoBehaviour {
+ 
+    public float speed = 2f;
+    public float yDirection = -1; //move from right to left
+   
+    // Use this for initialization
+    void Start () {
+    
+    }
+     
+    // Update is called once per frame
+    void Update () {
+        //move the root BG from right to left
+        transform.Translate(new Vector3(0, speed * yDirection * Time.deltaTime, 0));
+        if (transform.position.y <= -10) {
+            transform.position = new Vector3(0,0,0);
+        }
+    }   
+}
