f4d23aa96f570428deb0c63ecddb43f3f32546cd
Tekknobot <thrillatron@hotmail.com>
2020-12-13 08:31:15 -0500

    Add repeating background.

---
+++ Assets/Script/ScrollScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollScript : MonoBehaviour
+{
+    public float scrollSpeed = -1f;
+    float spriteSize;
+    Vector2 startPos;
+
+    void Start() {
+        startPos = new Vector2(0, -85f);
+        spriteSize = gameObject.GetComponent<SpriteRenderer>().bounds.size.y;
+    }
+
+    void Update() {
+        float newPos = Mathf.Repeat (Time.time * scrollSpeed, spriteSize);
+        transform.position = startPos + Vector2.up * newPos;
+    }
+}
