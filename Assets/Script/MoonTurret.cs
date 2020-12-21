e82d6892cf743c7cb24ea8fda27017e818523418
Tekknobot <thrillatron@hotmail.com>
2020-12-21 09:05:40 -0500

    Add Moon Turret asset.

---
+++ Assets/Script/MoonTurret.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoonTurret : MonoBehaviour
+{
+    public GameObject primaryWeapon;
+    public GameObject trigger;
+    public GameObject bomber;
+    public GameObject guards;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (trigger == null) {
+            gameObject.GetComponent<SmoothFollow>().enabled = true;
+            gameObject.GetComponent<BoxCollider2D>().enabled = true;
+            bomber.SetActive(true);
+            guards.SetActive(true);
+            primaryWeapon.SetActive(true);
+        }
+
+        if (bomber.GetComponent<bossTarget>().currentHealth <= 0) {
+            gameObject.GetComponent<ObjectOscillator>().enabled = true;
+            gameObject.GetComponent<ObjectOscillator>().speedMult = 5f;
+            gameObject.GetComponent<ObjectOscillator>().rangeMult = 4f;
+        }        
+    }
+}
