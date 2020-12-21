6e939cbce682c92028f8afc9c4f37cfe77156909
Tekknobot <thrillatron@hotmail.com>
2020-12-21 06:39:22 -0500

    Update Level 3 assets.

---
+++ Assets/Script/shuttle.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shuttle : MonoBehaviour
+{
+    public GameObject primaryWeapon;
+    public GameObject guardLeft;
+    public GameObject guardMiddle;
+    public GameObject guardRight;
+    public GameObject guardBottom;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+                   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (guardLeft == null && guardMiddle == null && guardRight == null && guardBottom == null) {
+            gameObject.GetComponent<SmoothFollow>().enabled = true;
+            gameObject.GetComponent<BoxCollider2D>().enabled = true;             
+        }
+
+        if (gameObject.GetComponent<bossTarget>().currentHealth <= 0f) {
+            primaryWeapon.SetActive(false);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "triggerBox") {
+            primaryWeapon.SetActive(true);
+        }
+    }    
+}
